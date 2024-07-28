c40389e62c57729e694008ed2f13bee22dc3ae8f
wanjohi lewis <wanjohilewi7@gmail.com>
2024-07-28 17:35:11 +0300

    producer part

---
+++ src/main/java/DevLewi/Apache_producer/ApacheProducerApplication.java
@@ -0,0 +1,13 @@
+package DevLewi.Apache_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DevLewi/Apache_producer/ApacheProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package DevLewi.Apache_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
